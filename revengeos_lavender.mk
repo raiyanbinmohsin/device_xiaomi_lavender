f5dc9722f427bcbd522ab15c57e7712d419c5f2d
mukulsharma05175 <73372165+mukulsharma05175@users.noreply.github.com>
2021-06-19 13:07:08 +0530

    Update revengeos_lavender.mk

--- revengeos_lavender.mk
+++ revengeos_lavender.mk
@@ -19,10 +19,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-# Inherit some common RevengeOS stuff
-$(call inherit-product, vendor/revengeos/config/common.mk)
+# Inherit some common asopOS stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
-REVENGEOS_BUILDTYPE := OFFICIAL
 
 # Inherit from lavender device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -30,14 +29,22 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := lavender
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := revengeos_lavender
+PRODUCT_NAME := asop_lavender
 PRODUCT_MODEL := Redmi Note 7
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
+TARGET_USES_BLUR := Fals
+TARGET_INCLUDE_PIXEL_CHARGER := true
+EXTRA_FOD_ANIMATIONS := true
+TARGET_USES_FACE_UNLOCK := true
+HYCON_MAINTAINER := Mukul_Sharma
 TARGET_VENDOR_PRODUCT_NAME := lavender
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210605.005 7349499 release-keys"
 
 BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210605.005/7349499:user/release-keys
+
+#official
+HYCON_BUILD_TYPE=UNOFFICIAL
